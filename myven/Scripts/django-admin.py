1cd28b5622c7d5f1d4f593f92ba333b55aae3ff2
wondora <wondora@yandex.com>
2021-02-02 15:13:05 +0900

    2-2 add venv

--- myven/Scripts/django-admin.py
+++ myven/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!g:\code\infogigi\myven\scripts\python.exe
+#!g:\code\info\myven\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
--- myven/Scripts/runxlrd.py
+++ myven/Scripts/runxlrd.py
@@ -1,4 +1,4 @@
-#!g:\code\infogigi\myven\scripts\python.exe
+#!g:\code\info\myven\scripts\python.exe
 # Copyright (c) 2005-2012 Stephen John Machin, Lingfo Pty Ltd
 # This script is part of the xlrd package, which is released under a
 # BSD-style licence.
