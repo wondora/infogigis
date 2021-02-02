0d8886cffce53aefa11939d3f0c80ac1d12321d0
wondora <wondora@yandex.com>
2021-02-02 15:48:21 +0900

    2-2 test3

--- gshs/settings/deploy copy.py
+++ gshs/settings/deploy copy.py
@@ -1,6 +1,11 @@
 from .base import *
 import os, environ
 
+# def read_secret(secret_name):
+#     with open('/run/secrets/' + secret_name) as f:
+#         secret = f.read()
+#         secret = secret.strip()
+#     return secret 
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -10,6 +15,7 @@ environ.Env.read_env(
     env_file=os.path.join(BASE_DIR, '.env')
 )
 
+# SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 SECRET_KEY = "0lb#yu$t1r8_+0g-yt33@y)ge2(&+4_$r84&hi(7#tz3l^yo21"
 DEBUG = False
 
