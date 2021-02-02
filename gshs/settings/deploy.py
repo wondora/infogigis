f06c6370b8e801e4c44a515fcd3ef023b56d1ac5
wondora <wondora@yandex.com>
2021-02-02 16:54:53 +0900

    2-2 modify some

--- gshs/settings/deploy.py
+++ gshs/settings/deploy.py
@@ -1,6 +1,11 @@
 from .base import *
 import os, environ
 
+def read_secret(secret_name):
+    with open('/run/secrets/' + secret_name) as f:
+        secret = f.read()
+        secret = secret.strip()
+    return secret 
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -10,7 +15,8 @@ environ.Env.read_env(
     env_file=os.path.join(BASE_DIR, '.env')
 )
 
-SECRET_KEY = "0lb#yu$t1r8_+0g-yt33@y)ge2(&+4_$r84&hi(7#tz3l^yo21"
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
@@ -20,7 +26,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'infogigi',
         'USER':'wondora',
-        'PASSWORD':'MYSQL_PASSWORD',
+        'PASSWORD':read_secret('MYSQL_PASSWORD'),
         'HOST':'mariadb',
         'PORT':'3306',
     }
