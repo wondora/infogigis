1f387e8f1f676f27b671b547a7945d93f2f9fc1e
wondora <wondora@yandex.com>
2021-02-08 18:54:54 +0900

    2-8 modify settings

--- gshs/settings/base.py
+++ gshs/settings/base.py
@@ -114,6 +114,9 @@ USE_TZ = False
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
